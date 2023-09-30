a8e00a77db8e5e2458b40bb2d612b9a3883564d0
Marc Ernest Mariano <78241292+marcernest21@users.noreply.github.com>
2023-10-01 04:28:48 +0800

    Sleep Analysis Structure

--- lib/screens/sleep_analysis_screen.dart
+++ lib/screens/sleep_analysis_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:smart_baby_cradle/widgets/sleep_analysis_item.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_baby_cradle/theme_provider.dart';
-// Import the SleepAnalysisItem widget
 
 class SleepAnalysisScreen extends StatelessWidget {
   static const routeName = '/sleep-analysis';
@@ -12,31 +11,185 @@ class SleepAnalysisScreen extends StatelessWidget {
     final themeProvider = Provider.of<ThemeProvider>(context);
     final currentTheme = themeProvider.currentTheme;
 
+    // Get the current time and date
+    final now = DateTime.now();
+    final formattedDate = '${now.year}-${now.month}-${now.day}';
+    final formattedTime = '${now.hour}:${now.minute}';
+
+    // Replace this value with your actual sleep quality percentage
+    final sleepQualityPercentage = 85.0;
+
+    // Replace this value with the actual sleep duration
+    final sleepDurationHours = 8;
+    final sleepDurationMinutes = 30;
+
     return Theme(
-        data: currentTheme,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Sleep Analysis'),
-          ),
-          body: const Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Welcome to Sleep Analysis!',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
+      data: currentTheme,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sleep Analysis'),
+        ),
+        body: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.start, // Align content at the top
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            // Rectangle with gradient background for current time and date
+            Container(
+              width: double.infinity, // Occupies the whole width
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue, Colors.purple],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
                 ),
-                SizedBox(
-                    height:
-                        20), // You can include the SleepAnalysisItem widget here
-                SizedBox(height: 20),
-                // Add more content here as needed
-              ],
+              ),
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Date: $formattedDate', // Display the formatted date
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.wb_sunny, // You can replace with a moon icon
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Time: $formattedTime', // Display the formatted time
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ));
+            SizedBox(height: 20),
+            // Rounded rectangle with gradient background for sleep quality
+            Container(
+              width: 300, // Adjust the width as needed
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  colors: [Colors.green, Colors.teal],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Sleep Quality',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.star, // You can replace with an appropriate icon
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  // Stack to place the percentage text inside the progress indicator
+                  Stack(
+                    alignment: Alignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 80,
+                        height: 80,
+                        child: CircularProgressIndicator(
+                          value: sleepQualityPercentage / 100.0,
+                          strokeWidth: 8,
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                        ),
+                      ),
+                      Text(
+                        '${sleepQualityPercentage.round()}%', // Display the sleep quality percentage
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            // Rounded rectangle with gradient background for sleep duration
+            Container(
+              width: 300, // Adjust the width as needed
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  colors: [Colors.orange, Colors.deepOrange],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Sleep Duration',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.access_time, // You can replace with a clock icon
+                        color: Colors.white,
+                        size: 32,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '$sleepDurationHours hours $sleepDurationMinutes minutes', // Replace with the actual sleep duration
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
