221b1f969c3b516b4cf856a5a6bd7aa0d7412f1d
cob-byte <jebarcelona2020@plm.edu.ph>
2023-10-02 03:43:59 +0800

    google/facebook login
    google lang working atm

--- lib/widgets/social.dart
+++ lib/widgets/social.dart
@@ -1,5 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
+final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+class Authentication {
+  static Future<User?> signInWithGoogle({required BuildContext context}) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    final GoogleSignIn googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount? googleSignInAccount =
+    await googleSignIn.signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      try {
+        final UserCredential userCredential =
+        await auth.signInWithCredential(credential);
+
+        user = userCredential.user;
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'account-exists-with-different-credential') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            Authentication.customSnackBar(
+              content:
+              'The account already exists with a different credential.',
+            ),
+          );
+        } else if (e.code == 'invalid-credential') {
+          ScaffoldMessenger.of(context).showSnackBar(
+            Authentication.customSnackBar(
+              content:
+              'Error occurred while accessing credentials. Try again.',
+            ),
+          );
+        }
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          Authentication.customSnackBar(
+            content: 'Error occurred using Google Sign-In. Try again.',
+          ),
+        );
+      }
+    }
+
+    return user;
+  }
+
+  static Future<UserCredential> signInWithFacebook() async {
+
+    final LoginResult loginResult = await FacebookAuth.instance.login(
+        permissions: [
+          'email', 'public_profile', 'user_birthday'
+        ]
+    );
+
+    final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
+
+  static SnackBar customSnackBar({required String content}) {
+    return SnackBar(
+      backgroundColor: Colors.white,
+      content: Text(
+        content,
+        style: TextStyle(color: Colors.black, letterSpacing: 0.5),
+      ),
+    );
+  }
+}
 
 class Social extends StatelessWidget {
   const Social({Key? key}) : super(key: key);
@@ -9,36 +91,61 @@ class Social extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Container(
-          width: 40,
-          height: 40,
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 226, 165, 104),
-            shape: BoxShape.circle,
-          ),
-          child: const Center(
-            child: Icon(
-              FontAwesomeIcons.facebookF,
-              color: Colors.white,
-              size: 20,
+        GestureDetector(
+          onTap: () {
+            Authentication.signInWithFacebook();
+          },
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 226, 165, 104),
+              shape: BoxShape.circle,
+            ),
+            child: const Center(
+              child: Icon(
+                FontAwesomeIcons.facebookF,
+                color: Colors.white,
+                size: 20,
+              ),
             ),
           ),
         ),
         const SizedBox(
           width: 10,
         ),
-        Container(
-          width: 40,
-          height: 40,
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 226, 165, 104),
-            shape: BoxShape.circle,
-          ),
-          child: const Center(
-            child: Icon(
-              FontAwesomeIcons.google,
-              color: Colors.white,
-              size: 20,
+        GestureDetector(
+          onTap: () async {
+            User? user = await Authentication.signInWithGoogle(context: context);
+            if (user != null) {
+              // User is signed in
+              ScaffoldMessenger.of(context).showSnackBar(
+                Authentication.customSnackBar(
+                  content: 'User signed in: ${user.displayName}',
+                ),
+              );
+            } else {
+              // User is not signed in
+              ScaffoldMessenger.of(context).showSnackBar(
+                Authentication.customSnackBar(
+                  content: 'User not signed in.',
+                ),
+              );
+            }
+          },
+          child: Container(
+            width: 40,
+            height: 40,
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 226, 165, 104),
+              shape: BoxShape.circle,
+            ),
+            child: const Center(
+              child: Icon(
+                FontAwesomeIcons.google,
+                color: Colors.white,
+                size: 20,
+              ),
             ),
           ),
         ),
