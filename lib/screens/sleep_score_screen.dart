994ac57ffe5bef8231736d697e66ca436f00cf3a
cob-byte <jebarcelona2020@plm.edu.ph>
2023-12-06 03:12:12 +0800

    overall sleep quality metrics

--- lib/screens/sleep_score_screen.dart
+++ lib/screens/sleep_score_screen.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:smart_baby_cradle/screens/sleep_pattern_screen.dart';
 import 'package:smart_baby_cradle/screens/sleep_efficiency_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:smart_baby_cradle/screens/wake_up_times_screen.dart';
+import '../services/status_service.dart';
 import '../theme_provider.dart';
 import 'package:intl/intl.dart';
 
@@ -78,6 +81,66 @@ class SleepScoreScreen extends StatelessWidget {
   }
 }
 
+Future<Map<String, List<SleepInfo>>> _getSleepInfo() async {
+  String? deviceID = await auth.getDeviceID();
+
+  Map<String, List<SleepInfo>> sleepInfos = {};
+
+  if (deviceID != null) {
+    // Fetch sleep info data from Firebase
+    DataSnapshot snapshot;
+    try {
+      snapshot = (await FirebaseDatabase.instance
+          .ref()
+          .child("devices")
+          .child(deviceID)
+          .child("tracker")
+          .once()).snapshot;
+    } catch (e) {
+      print("Failed to fetch data from Firebase: $e");
+      return sleepInfos;
+    }
+
+    if (snapshot.value != null) {
+      Map<dynamic, dynamic> sleepData = snapshot.value as Map<dynamic, dynamic>;
+      sleepData.forEach((key, value) {
+        try {
+          // key is the date
+          DateTime date = DateTime.parse(key);
+          String dayOfWeek = DateFormat('EEEE').format(date); // 'EEEE' gives full name of the day of week ex Monday Tuesday wed etc
+          // value is a map of timestamps
+          Map<dynamic, dynamic> timestamps = value as Map<dynamic, dynamic>;
+          timestamps.forEach((timestamp, sleepInfoData) {
+            // Convert data from Firebase to SleepInfo object
+            SleepInfo info = SleepInfo(
+              _parseTimeOfDay(sleepInfoData['timePutToBed']),
+              _parseTimeOfDay(sleepInfoData['timeFellAsleep']),
+              _parseTimeOfDay(sleepInfoData['wakeUpTime']),
+            );
+
+            // If there's no list for this day of the week, create one
+            if (!sleepInfos.containsKey(dayOfWeek)) {
+              sleepInfos[dayOfWeek] = [];
+            }
+
+            // Add the SleepInfo object to the list for this day of the week
+            sleepInfos[dayOfWeek]!.add(info);
+          });
+        } catch (e) {
+          print("Failed to parse sleep info data: $e");
+        }
+      });
+    }
+  }
+
+  return sleepInfos;
+}
+
+TimeOfDay _parseTimeOfDay(String timeString) {
+  List<String> parts = timeString.split(':');
+  return TimeOfDay(hour: int.parse(parts[0]), minute: int.parse(parts[1]));
+}
+
 class SleepQualityCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -88,54 +151,96 @@ class SleepQualityCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text('Sleep Quality Metrics',
+            Text('Overall Sleep Quality Metrics',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
             SizedBox(height: 16),
-            SleepInfoRow(
-                icon: Icons.access_time,
-                label: 'Sleep Efficiency',
-                value: '85%'),
-            SizedBox(height: 16),
-            Container(
-              height: 200,
-              child: Row(
-                children: [
-                  Expanded(
-                    child: PieChart(
-                      PieChartData(
-                        sections: [
-                          PieChartSectionData(
-                            value: 70,
-                            color: Colors.blue,
-                            title: 'Sleep',
-                            radius: 50,
-                          ),
-                          PieChartSectionData(
-                            value: 30,
-                            color: Colors.red,
-                            title: 'In Bed',
-                            radius: 50,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: 16),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+            FutureBuilder<Map<String, List<SleepInfo>>>(
+              future: _getSleepInfo(),
+              builder: (BuildContext context, AsyncSnapshot<Map<String, List<SleepInfo>>> snapshot) {
+                if (snapshot.hasData) {
+                  Map<String, List<SleepInfo>> sleepInfos = snapshot.data!;
+                  double totalHoursOfSleep = 0;
+                  double totalHoursInCradle = 0;
+                  int totalAwakenings = 0;
+
+                  sleepInfos.forEach((day, infos) {
+                    infos.forEach((info) {
+                      Duration fellAsleep = Duration(hours: info.timeFellAsleep.hour, minutes: info.timeFellAsleep.minute);
+                      Duration wokeUp = Duration(hours: info.wakeUpTime.hour, minutes: info.wakeUpTime.minute);
+                      if (wokeUp < fellAsleep) {
+                        wokeUp += Duration(hours: 24);
+                      }
+                      totalHoursOfSleep += (wokeUp.inMinutes - fellAsleep.inMinutes) / 60;
+
+                      Duration putToBed = Duration(hours: info.timePutToBed.hour, minutes: info.timePutToBed.minute);
+                      if (wokeUp < putToBed) {
+                        wokeUp += Duration(hours: 24);
+                      }
+                      totalHoursInCradle += (wokeUp.inMinutes - putToBed.inMinutes) / 60;
+
+                      totalAwakenings++;
+                    });
+                  });
+
+                  double sleepEfficiency = (totalHoursOfSleep / totalHoursInCradle) * 100;
+
+                  return Column(
                     children: [
-                      PieChartPercentage(label: 'Sleep', percentage: '70%'),
-                      PieChartPercentage(label: 'In Bed', percentage: '30%'),
+                      SleepInfoRow(
+                          icon: Icons.access_time,
+                          label: 'Sleep Efficiency',
+                          value: '${sleepEfficiency.toStringAsFixed(1)}%'),
+                      SizedBox(height: 16),
+                      Container(
+                        height: 200,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: PieChart(
+                                PieChartData(
+                                  sections: [
+                                    PieChartSectionData(
+                                      value: totalHoursOfSleep,
+                                      color: Colors.blue,
+                                      title: 'Sleep',
+                                      radius: 50,
+                                    ),
+                                    PieChartSectionData(
+                                      value: totalHoursInCradle,
+                                      color: Colors.red,
+                                      title: 'In Bed',
+                                      radius: 50,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                PieChartPercentage(label: 'Sleep', percentage: '${(totalHoursOfSleep / (totalHoursOfSleep + totalHoursInCradle) * 100).toStringAsFixed(1)}%'),
+                                PieChartPercentage(label: 'In Bed', percentage: '${(totalHoursInCradle / (totalHoursOfSleep + totalHoursInCradle) * 100).toStringAsFixed(1)}%'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      SleepInfoRow(
+                          icon: Icons.nightlight_round,
+                          label: 'Total Tracked Sleep',
+                          value: '$totalAwakenings'),
                     ],
-                  ),
-                ],
-              ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  // Return a loading indicator while waiting for the future to complete
+                  return CircularProgressIndicator();
+                }
+              },
             ),
-            SizedBox(height: 16),
-            SleepInfoRow(
-                icon: Icons.nightlight_round,
-                label: 'Total Awakenings',
-                value: '3'),
             SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: () {
